bf7ed8e59b62256fa70d7ceef2dbafbde7b09d0f
张浩 <halzhan@zhanghaodeMBP.lan>
2017-02-26 18:59:01 +0800

    pipes

---
+++ src/009-pipes/src/app/fetch-json.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Http }                from '@angular/http';
+@Pipe({
+  name: 'fetch',
+  pure: false
+})
+export class FetchJsonPipe  implements PipeTransform {
+  private fetchedJson: any = null;
+  private prevUrl = '';
+  constructor(private _http: Http) { }
+  transform(url: string): any {
+    if (url !== this.prevUrl) {
+      this.prevUrl = url;
+      this.fetchedJson = null;
+      this._http.get(url)
+        .map( result => result.json() )
+        .subscribe( result => this.fetchedJson = result );
+    }
+    return this.fetchedJson;
+  }
+}
---
+++ src/009-pipes/src/app/flying-heroes.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { Flyer } from './heroes';
+
+@Pipe({ name: 'flyingHeroes' })
+export class FlyingHeroesPipe implements PipeTransform {
+  transform(allHeroes: Flyer[]) {
+    return allHeroes.filter(hero => hero.canFly);
+  }
+}
+
+@Pipe({
+  name: 'flyingHeroesImpure',
+  pure: false
+})
+export class FlyingHeroesImpurePipe extends FlyingHeroesPipe {}
