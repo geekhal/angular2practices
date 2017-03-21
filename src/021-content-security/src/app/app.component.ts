dc2678561bdd7e34d99915663525d5b9d283859c
张浩 <halzhan@zhanghaodeMacBook-Pro.local>
2017-03-21 12:01:30 +0800

    security

---
+++ src/021-content-security/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>Security</h1>
+  <inner-html-binding></inner-html-binding>
+  <bypass-security></bypass-security>
+  `
+})
+export class AppComponent {
+}
---
+++ src/021-content-security/src/app/inner-html-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: `${module.id}`,
+  selector: 'inner-html-binding',
+  templateUrl: './inner-html-binding.component.html',
+})
+export class InnerHtmlBindingComponent {
+  // For example, a user/attacker-controlled value from a URL.
+  htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
+}
